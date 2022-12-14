61e6c974213caf943f4395ff2ab654485314c0d4
ItzAreeb <areebshahid800@gmail.com>
2022-12-13 19:56:35 -0500

    Gravity Guy Game

---
+++ Club/Godot Game #1/src/Actors/Actor.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+class_name Actor
+export var gravity: = 2500.0
+export var UP = Vector2(0.0,-1.0)
+var _velocity: = Vector2()
+var direction = 0
+var speed: = 400.0
+const MAX_SPEED = 600
+const ACCELERATION = 1000
+const DECELERATION = 2000
+	
